825b4190087e660d23b2f54d840831f9d4968121
Gabriel Jamrewav <gabriel.jamrewav@gmail.com>
2022-12-06 16:59:50 +0800

    lazy react

--- src/Router.tsx
+++ src/Router.tsx
@@ -33,82 +33,94 @@ const Router: React.FC = () => {
     }, 2000);
   }
 
-  return loading == false ? (
-    <Suspense fallback={renderLoader()}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<LandingPage />} />
-          <Route path="/auth" element={<Auth />}>
-            <Route path="/auth/login" element={<Login />}></Route>
-            <Route path="/auth/register" element={<Register />}></Route>
-            <Route path="/auth/forgotPassword" element={<Login />}></Route>
-          </Route>
+  if (loading) return <></>;
 
-          <Route path="/dashboard-user" element={<DashboardUser />}>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="/auth" element={<Auth />}>
+          <Route
+            path="/auth/login"
+            element={
+              <Suspense fallback={renderLoader()}>
+                <Login />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/auth/register"
+            element={
+              <Suspense fallback={renderLoader()}>
+                <Register />
+              </Suspense>
+            }
+          ></Route>
+          <Route path="/auth/forgotPassword" element={<Login />}></Route>
+        </Route>
+
+        <Route path="/dashboard-user" element={<DashboardUser />}>
+          <Route
+            path="/dashboard-user/home-page"
+            element={<HomePage />}
+          ></Route>
+          <Route
+            path="/dashboard-user/home-page/result-page"
+            element={<ResultPage />}
+          ></Route>
+          <Route
+            path="/dashboard-user/home-page/article-page"
+            element={<ArticlePage />}
+          ></Route>
+          <Route
+            path="/dashboard-user/activity-page"
+            element={<ActivityPage />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/1"
+            element={<MencariNarasumberPage1 />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/2"
+            element={<MencariNarasumberPage2 />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/3"
+            element={<MencariNarasumberPage3 />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/4"
+            element={<MencariNarasumberPage4 />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/4/wait"
+            element={<MenungguPembayaran />}
+          ></Route>
+          <Route
+            path="/dashboard-user/mencari-narasumber/5"
+            element={<MencariNarasumberPage5 />}
+          ></Route>
+          <Route path="/dashboard-user/profil-user" element={<ProfileUser />}>
             <Route
-              path="/dashboard-user/home-page"
-              element={<HomePage />}
+              path="/dashboard-user/profil-user/akun"
+              element={<AkunSaya />}
             ></Route>
             <Route
-              path="/dashboard-user/home-page/result-page"
-              element={<ResultPage />}
+              path="/dashboard-user/profil-user/edit"
+              element={<EditProfile />}
             ></Route>
             <Route
-              path="/dashboard-user/home-page/article-page"
-              element={<ArticlePage />}
+              path="/dashboard-user/profil-user/reset"
+              element={<GantiSandi />}
             ></Route>
             <Route
-              path="/dashboard-user/activity-page"
-              element={<ActivityPage />}
+              path="/dashboard-user/profil-user/menjadi-narasumber"
+              element={<MenjadiNarasumber />}
             ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/1"
-              element={<MencariNarasumberPage1 />}
-            ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/2"
-              element={<MencariNarasumberPage2 />}
-            ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/3"
-              element={<MencariNarasumberPage3 />}
-            ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/4"
-              element={<MencariNarasumberPage4 />}
-            ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/4/wait"
-              element={<MenungguPembayaran />}
-            ></Route>
-            <Route
-              path="/dashboard-user/mencari-narasumber/5"
-              element={<MencariNarasumberPage5 />}
-            ></Route>
-            <Route path="/dashboard-user/profil-user" element={<ProfileUser />}>
-              <Route
-                path="/dashboard-user/profil-user/akun"
-                element={<AkunSaya />}
-              ></Route>
-              <Route
-                path="/dashboard-user/profil-user/edit"
-                element={<EditProfile />}
-              ></Route>
-              <Route
-                path="/dashboard-user/profil-user/reset"
-                element={<GantiSandi />}
-              ></Route>
-              <Route
-                path="/dashboard-user/profil-user/menjadi-narasumber"
-                element={<MenjadiNarasumber />}
-              ></Route>
-            </Route>
           </Route>
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
-  ) : (
-    <></>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
