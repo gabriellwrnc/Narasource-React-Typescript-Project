f8355ce5e63ad55ecb31934f446a8fbb667e3d90
Gabriel Jamrewav <gabriel.jamrewav@gmail.com>
2022-12-06 16:43:49 +0800

    lazy react

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth, DashboardUser, ProfileUser } from "./layouts";
 import {
@@ -9,7 +9,6 @@ import {
   GantiSandi,
   HomePage,
   LandingPage,
-  Login,
   MencariNarasumberPage1,
   MencariNarasumberPage2,
   MencariNarasumberPage3,
@@ -17,9 +16,12 @@ import {
   MencariNarasumberPage5,
   MenjadiNarasumber,
   MenungguPembayaran,
-  Register,
   ResultPage,
 } from "./screens";
+const Login = lazy(() => import("./screens/Auth/Login/Login"));
+const Register = lazy(() => import("./screens/Auth/Register/Register"));
+
+const renderLoader = () => <p>Loading</p>;
 
 const Router: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -32,77 +34,79 @@ const Router: React.FC = () => {
   }
 
   return loading == false ? (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<LandingPage />} />
-        <Route path="/auth" element={<Auth />}>
-          <Route path="/auth/login" element={<Login />}></Route>
-          <Route path="/auth/register" element={<Register />}></Route>
-          <Route path="/auth/forgotPassword" element={<Login />}></Route>
-        </Route>
-
-        <Route path="/dashboard-user" element={<DashboardUser />}>
-          <Route
-            path="/dashboard-user/home-page"
-            element={<HomePage />}
-          ></Route>
-          <Route
-            path="/dashboard-user/home-page/result-page"
-            element={<ResultPage />}
-          ></Route>
-          <Route
-            path="/dashboard-user/home-page/article-page"
-            element={<ArticlePage />}
-          ></Route>
-          <Route
-            path="/dashboard-user/activity-page"
-            element={<ActivityPage />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/1"
-            element={<MencariNarasumberPage1 />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/2"
-            element={<MencariNarasumberPage2 />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/3"
-            element={<MencariNarasumberPage3 />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/4"
-            element={<MencariNarasumberPage4 />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/4/wait"
-            element={<MenungguPembayaran />}
-          ></Route>
-          <Route
-            path="/dashboard-user/mencari-narasumber/5"
-            element={<MencariNarasumberPage5 />}
-          ></Route>
-          <Route path="/dashboard-user/profil-user" element={<ProfileUser />}>
-            <Route
-              path="/dashboard-user/profil-user/akun"
-              element={<AkunSaya />}
-            ></Route>
-            <Route
-              path="/dashboard-user/profil-user/edit"
-              element={<EditProfile />}
-            ></Route>
-            <Route
-              path="/dashboard-user/profil-user/reset"
-              element={<GantiSandi />}
-            ></Route>
-            <Route
-              path="/dashboard-user/profil-user/menjadi-narasumber"
-              element={<MenjadiNarasumber />}
-            ></Route>
+    <Suspense fallback={renderLoader()}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path="/auth" element={<Auth />}>
+            <Route path="/auth/login" element={<Login />}></Route>
+            <Route path="/auth/register" element={<Register />}></Route>
+            <Route path="/auth/forgotPassword" element={<Login />}></Route>
           </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+
+          <Route path="/dashboard-user" element={<DashboardUser />}>
+            <Route
+              path="/dashboard-user/home-page"
+              element={<HomePage />}
+            ></Route>
+            <Route
+              path="/dashboard-user/home-page/result-page"
+              element={<ResultPage />}
+            ></Route>
+            <Route
+              path="/dashboard-user/home-page/article-page"
+              element={<ArticlePage />}
+            ></Route>
+            <Route
+              path="/dashboard-user/activity-page"
+              element={<ActivityPage />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/1"
+              element={<MencariNarasumberPage1 />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/2"
+              element={<MencariNarasumberPage2 />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/3"
+              element={<MencariNarasumberPage3 />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/4"
+              element={<MencariNarasumberPage4 />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/4/wait"
+              element={<MenungguPembayaran />}
+            ></Route>
+            <Route
+              path="/dashboard-user/mencari-narasumber/5"
+              element={<MencariNarasumberPage5 />}
+            ></Route>
+            <Route path="/dashboard-user/profil-user" element={<ProfileUser />}>
+              <Route
+                path="/dashboard-user/profil-user/akun"
+                element={<AkunSaya />}
+              ></Route>
+              <Route
+                path="/dashboard-user/profil-user/edit"
+                element={<EditProfile />}
+              ></Route>
+              <Route
+                path="/dashboard-user/profil-user/reset"
+                element={<GantiSandi />}
+              ></Route>
+              <Route
+                path="/dashboard-user/profil-user/menjadi-narasumber"
+                element={<MenjadiNarasumber />}
+              ></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   ) : (
     <></>
   );
